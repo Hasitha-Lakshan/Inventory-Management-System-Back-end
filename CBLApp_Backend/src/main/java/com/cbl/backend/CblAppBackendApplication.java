18d7557b8e824e8b6d25189ed5cb30c8e0424687
Hasitha <ranaweerahlakshan@gmail.com>
2020-07-14 15:17:07 +0530

    Created the spring starter project with dependency

---
+++ CBLApp_Backend/src/main/java/com/cbl/backend/CblAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cbl.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CblAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CblAppBackendApplication.class, args);
+	}
+
+}
