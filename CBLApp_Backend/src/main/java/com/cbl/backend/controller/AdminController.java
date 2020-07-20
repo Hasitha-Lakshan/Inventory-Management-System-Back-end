ad486b814f13744d5e7e0a762628424e3825c4e0
Kansaji <lakruwan45@gmail.com>
2020-07-20 22:35:34 +0530

    added update user info, delete user, set account status features

--- CBLApp_Backend/src/main/java/com/cbl/backend/controller/AdminController.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/controller/AdminController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cbl.backend.dto.UserDetailsRequest;
+import com.cbl.backend.dto.UserInfoUpdateRequest;
 import com.cbl.backend.service.AdminService;
 
 @RestController
@@ -24,4 +30,43 @@ public class AdminController {
 		
 		return new ResponseEntity<>(adminService.getAllUsers(), HttpStatus.OK);
 	}
+	
+	
+	@PutMapping("/updateuser")
+	public ResponseEntity updateUserInfo(@RequestBody UserInfoUpdateRequest rq) {
+		
+		UserInfoUpdateRequest Obj = adminService.updateUserInfo(rq);
+		if(Obj!=null) {
+			return new ResponseEntity<>(rq, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(rq, HttpStatus.BAD_REQUEST);
+		}
+		
+			
+		
+	} 
+	
+	@PutMapping("/setaccountstatus/{id}")
+	public ResponseEntity swapAccountStatus(@PathVariable int id) {
+			
+		boolean succesful = adminService.swapAccountStatus(id);
+		if(succesful) {
+			return new ResponseEntity<>(succesful, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(succesful, HttpStatus.BAD_REQUEST);
+		}
+	}	
+	
+	@DeleteMapping("/deleteuser/{id}")
+	public ResponseEntity deleteUserById(@PathVariable int id) {
+			
+		boolean succesful = adminService.deleteUserById(id);
+		if(succesful) {
+			return new ResponseEntity<>(succesful, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(succesful, HttpStatus.BAD_REQUEST);
+		}
+	}	
+	
+
 }
--- CBLApp_Backend/src/main/java/com/cbl/backend/service/AdminService.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/service/AdminService.java
@@ -1,12 +1,17 @@
 package com.cbl.backend.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cbl.backend.dto.UserDetailsRequest;
+import com.cbl.backend.dto.UserInfoUpdateRequest;
 import com.cbl.backend.model.User;
 import com.cbl.backend.repository.UserRepository;
 
@@ -39,5 +44,54 @@ public class AdminService {
 		userDetailsRequest.setPhoneNumbers(user.getPhoneNumbers());
 		
 		return userDetailsRequest;
+	}
+	
+	public UserInfoUpdateRequest updateUserInfo(@RequestBody UserInfoUpdateRequest rq) {
+		
+		User user = userRepository.findById(rq.getUserID()).orElse(null);
+		if(user!=null) {
+			/* Updatable attributes */
+			user.setFirstName(rq.getFirstName());
+			user.setLastName(rq.getLastName());
+			user.setAddressLine1(rq.getAddressLine1());
+			user.setAddressLine2(rq.getAddressLine2());
+			user.setAddressLine3(rq.getAddressLine3());
+			user.setAccountStatus(rq.isAccountStatus());
+			user.setRole(rq.getRole());
+			user.setPhoneNumbers(rq.getPhoneNumbers());
+			
+			/* Non updatable attributes */
+			user.setPassword(user.getPassword());
+			user.setUserID(user.getUserID());
+			user.setUsername(user.getUsername());
+			
+			userRepository.save(user);
+			return rq;
+		} 
+			return null;	
+		
+	}
+	
+	public boolean swapAccountStatus(int id) {
+		
+		User user = userRepository.findById(id).orElse(null);
+		if(user!=null) {
+			/*swap the boolean status of the user account status*/
+			user.setAccountStatus(!user.isAccountStatus());			
+			userRepository.save(user);
+			return true;
+		} 
+			return false;	
+	}
+	
+	public boolean deleteUserById(int id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		if(user!=null) {
+			userRepository.deleteById(id);
+			return true;  
+		}else {
+			return false;
+		}
 	}
 }
