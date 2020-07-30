1bfcb3833dd60b590deb2620e4a12ee2ddea8b7b
Arshana <44945456+arshana790@users.noreply.github.com>
2020-07-31 01:27:35 +0530

    added produc entity with relations

--- CBLApp_Backend/src/main/java/com/cbl/backend/model/Report.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/model/Report.java
@@ -1,12 +1,16 @@
 package com.cbl.backend.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
@@ -17,14 +21,14 @@ public class Report{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int reportID;
-	
 	@NotEmpty
 	@Column
 	private LocalDateTime dateTime;
-	
 	@NotEmpty
 	@Column
 	private String reportName;
+	@OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL, mappedBy = "report")
+	private List<Report> report;
 
 	public int getReportID() {
 		return reportID;
