d6b6c521744f8d36d6d2267aecbd2c02c2e7f8a2
Kavinda <kavindanimalaka97@gmail.com>
2020-07-31 16:01:39 +0530

    Modified the salesItemsReport class

--- CBLApp_Backend/src/main/java/com/cbl/backend/model/SalesItemsReport.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/model/SalesItemsReport.java
@@ -1,145 +1,67 @@
 package com.cbl.backend.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name="SalesItemsReport")
-public class SalesItemsReport {
+@Table(name = "SalesItemsReport")
+public class SalesItemsReport{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int reports_Id;
+	private int salesItemsReportID;
 	@NotEmpty
 	@Column
-	private Date date;
+	private LocalDateTime dateTime;
 	@NotEmpty
 	@Column
-	private String product_name;
-	@NotEmpty
-	@Column
-	private int unit_buying_price;
-	@NotEmpty
-	@Column
-	private int unit_selling_price;
-	@NotEmpty
-	@Column
-	private int quantity;
-	@NotEmpty
-	@Column
-	private int buying_price;
-	@NotEmpty
-	@Column
-	private int selling_price;
-	@NotEmpty
-	@Column
-	private int profit;
-	
-	
-	public int getReport_Id() {
-		return report_Id;
+	private String salesItemsReportName;
+	@OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL, mappedBy = "salesItemsReport")
+	private List<SalesItemsReport> salesItemsReport;
+
+	public int getSalesItemsReportID() {
+		return salesItemsReportID;
 	}
 
-
-	public void setReport_Id(int report_Id) {
-		this.report_Id = report_Id;
+	public void setSalesItemsReportID(int salesItemsReportID) {
+		this.salesItemsReportID = salesItemsReportID;
 	}
 
-
-	public Date getDate() {
-		return date;
+	public LocalDateTime getDateTime() {
+		return dateTime;
 	}
 
-
-	public void setDate(Date date) {
-		this.date = date;
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
 	}
 
-
-	public String getProduct_name() {
-		return product_name;
+	public String getSalesItemsReportName() {
+		return salesItemsReportName;
 	}
 
-
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
+	public void setSalesItemsReportName(String salesItemsReportName) {
+		this.salesItemsReportName = salesItemsReportName;
 	}
-
-
-	public int getUnit_buying_price() {
-		return unit_buying_price;
-	}
-
-
-	public void setUnit_buying_price(int unit_buying_price) {
-		this.unit_buying_price = unit_buying_price;
-	}
-
-
-	public int getUnit_selling_price() {
-		return unit_selling_price;
-	}
-
-
-	public void setUnit_selling_price(int unit_selling_price) {
-		this.unit_selling_price = unit_selling_price;
-	}
-
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-
-	public int getBuying_price() {
-		return buying_price;
-	}
-
-
-	public void setBuying_price(int buying_price) {
-		this.buying_price = buying_price;
-	}
-
-
-	public int getSelling_price() {
-		return selling_price;
-	}
-
-
-	public void setSelling_price(int selling_price) {
-		this.selling_price = selling_price;
-	}
-
-
-	public int getProfit() {
-		return profit;
-	}
-
-
-	public void setProfit(int profit) {
-		this.profit = profit;
-	}
-
 
 	@Override
 	public String toString() {
-		return "SalesItemsReport [reports_Id=" + reports_Id + ", date=" + date + ", product_name=" + product_name
-				+ ", unit_buying_price=" + unit_buying_price + ", unit_selling_price=" + unit_selling_price + ", quantity="
-				+ quantity + ", buying_price=" + buying_price + ", selling_price=" + selling_price + ", profit="
-				+ profit + "]";
+		return "SalesItemsReport [salesItemsReportID=" + salesItemsReportID + ", dateTime=" + dateTime + ", salesItemsReportName=" + salesItemsReport + "]";
 	}
 
-
+	
+	
+	
+		
+	
 }
