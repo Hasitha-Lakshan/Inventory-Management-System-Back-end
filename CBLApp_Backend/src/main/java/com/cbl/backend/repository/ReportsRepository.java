0de21fe8c3260d0e63763e75f420f48b2cf163b1
Kavinda <kavindanimalaka97@gmail.com>
2020-08-01 18:52:31 +0530

    created reportsRepository

--- CBLApp_Backend/src/main/java/com/cbl/backend/repository/ReportsRepository.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/repository/ReportsRepository.java
@@ -1,14 +1,11 @@
 package com.cbl.backend.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cbl.backend.model.Reports;
+import com.cbl.backend.model.SalesItemsReport;
 
 @Repository 
-public interface ReportsRepository extends JpaRepository<Reports, Integer>{
+public interface ReportsRepository extends JpaRepository<SalesItemsReport, Integer>{
 	
-	Optional<Reports> findByReportsdate (String product_name);
 }
