6f1eda10fe12d31e50c799a86e75876e54c63db5
sajithnaveen <64298522+sajithnaveen@users.noreply.github.com>
2020-08-05 12:50:16 +0530

    backend and test cases

---
+++ CBLApp_Backend/src/main/java/com/cbl/backend/dto/LorryRequest.java
@@ -0,0 +1,39 @@
+package com.cbl.backend.dto;
+
+public class LorryRequest {
+	
+	private String firstName;
+	private String lastName;
+	private int phonenumber;
+	private boolean accountStatus;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public boolean isAccountStatus() {
+		return accountStatus;
+	}
+	public void setAccountStatus(boolean accountStatus) {
+		this.accountStatus = accountStatus;
+	}
+	
+	
+	
+	
+}
