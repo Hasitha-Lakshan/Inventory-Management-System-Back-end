0dea23001fd5bffff5dc96d81157d73ba4854a7f
Hasitha <ranaweerahlakshan@gmail.com>
2020-08-08 16:26:06 +0530

    Small update

--- CBLApp_Backend/src/main/java/com/cbl/backend/controller/LorryController.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/controller/LorryController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +19,6 @@ import com.cbl.backend.dto.LorryResponse;
 import com.cbl.backend.service.LorryService;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-
 @RequestMapping("/api/inventoryManager/distribution/")
 public class LorryController {
 
