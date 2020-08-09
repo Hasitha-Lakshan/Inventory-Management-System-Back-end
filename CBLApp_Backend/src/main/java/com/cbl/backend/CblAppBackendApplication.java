5b8f90e6de16076ff3a864df97d878e12bb3df8d
Hasitha <ranaweerahlakshan@gmail.com>
2020-08-09 23:20:11 +0530

    Cleaned up codes!

--- CBLApp_Backend/src/main/java/com/cbl/backend/CblAppBackendApplication.java
+++ CBLApp_Backend/src/main/java/com/cbl/backend/CblAppBackendApplication.java
@@ -1,9 +1,7 @@
 package com.cbl.backend;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
